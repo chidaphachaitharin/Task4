ce65d5e63c3d177681263326c18dfda32c464206
mac2022 <jidapa17189@gmail.com>
2023-10-20 11:13:32 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        // Choose an odd number of Lego bricks between 50 and 100
-        int amountOfBricks = 71; // You can change this value as needed
-        // Choose an even number of Lego blocks that fit in one container between 5 and 10
-        int containerCapacity = 8; // You can change this value as needed
+
+        int amountOfBricks = 91;
+        int containerCapacity = 4;
 
         int fullContainers = amountOfBricks / containerCapacity;
         int blocksInIncompleteContainer = amountOfBricks % containerCapacity;
-        int totalContainers = (blocksInIncompleteContainer > 0) ? fullContainers + 1 : fullContainers;
+
+        int totalContainers = 0;
+        if (blocksInIncompleteContainer > 0) {
+            totalContainers = fullContainers + 1;
+        }
 
         System.out.println("Total number of Lego bricks: " + amountOfBricks);
         System.out.println("Number of Lego blocks that fit in one container: " + containerCapacity);
